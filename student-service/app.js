7b144b3d3ff14f6031e196ac23fc4ed1706aa5be
Hasitha-Su <hasithakms@gmail.com>
2022-01-21 23:39:34 +0530

    update app.js

--- student-service/app.js
+++ student-service/app.js
@@ -6,9 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-<<<<<<< HEAD
   res.end('Hello Blackcat');
->>>>>>> Feature-001
 });
 
 server.listen(port, hostname, () => {
